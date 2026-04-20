8d52f351ca0820feb9998e91f73f3d44ca4b455a
GabrielHome <lopezalvaradoa749@gmail.com>
2026-04-20 17:22:29 -0600

    Codigo corregido

--- app.py
+++ app.py
@@ -25,6 +25,7 @@ def hubspot_webhook():
     return jsonify({"status": "ok"}), 200
 
 def vincular_equipo(ticket_id, ns_value):
+
     ns_limpio = str(ns_value).strip()
     
     url_objeto = f"https://api.hubapi.com/crm/v3/objects/{OBJETO_EQUIPOS}/search"
@@ -39,31 +40,32 @@ def vincular_equipo(ticket_id, ns_value):
         }]
     }
     
-    print(f"Intentando buscar equipo con {PROP_EQUIPO_NS} = '{ns_limpio}'")
+    print(f"Buscando en: {OBJETO_EQUIPOS} | Propiedad: {PROP_EQUIPO_NS} | Valor: '{ns_limpio}'")
     
     respuesta = requests.post(url_objeto, headers=HEADERS, json=busqueda)
-    
-    datos_respuesta = respuesta.json()
-    
+    datos_api = respuesta.json()
+
     if respuesta.status_code != 200:
-        print(f"Error en la API de búsqueda: {datos_respuesta}")
+        print(f"ERROR EN API HUBSPOT: {datos_api}")
         return
 
-    resultado = datos_respuesta.get('results', [])
+    print(f"Respuesta de búsqueda (Total encontrados: {datos_api.get('total', 0)})")
+
+    resultado = datos_api.get('results', [])
 
     if resultado:
         equipo_id = resultado[0]['id']
-        print(f"Equipo encontrado ID: {equipo_id}. Vinculando")
+        print(f"Equipo encontrado ID: {equipo_id}. Asociando...")
         
         url_asociacion = f"https://api.hubapi.com/crm/v4/objects/tickets/{ticket_id}/associations/default/{OBJETO_EQUIPOS}/{equipo_id}"
         operacion_assoc = requests.put(url_asociacion, headers=HEADERS)
         
         if operacion_assoc.status_code in [200, 201]:
-            print(f"Ticket {ticket_id} vinculado a equipo {equipo_id}")
+            print(f"Vinculacion correcta: Ticket {ticket_id} <-> Equipo {equipo_id}")
         else:
-            print(f"Error en asociación: {operacion_assoc.status_code} - {operacion_assoc.text}")
+            print(f"Fallo al vincular: {operacion_assoc.text}")
     else:
-        print(f"...Equipo no encontrado...")
+        print(f"Equipo con n.serie \"{ns_limpio} no encontrado\"")
 
 if __name__ == '__main__':
     port = int(os.environ.get("PORT", 10000))
